aad21ed827425426f205efc3b89bfbd93156f20d
Five <f5.chjr@gmail.com>
2019-12-07 21:38:59 +0700

    chore(app): change default color

--- app/src/app/per-event/per-event.page.ts
+++ app/src/app/per-event/per-event.page.ts
@@ -9,6 +9,7 @@ import { Subject, BehaviorSubject } from 'rxjs';
 })
 export class PerEventPage implements OnInit {
     public navigationBackground: BehaviorSubject<boolean>;
+    public fallbackBackground: string = '#dfdfdf';
 
     @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
         const scrollTop = $event.target.scrollingElement.scrollTop;
@@ -31,5 +32,21 @@ export class PerEventPage implements OnInit {
 
     goBack() {
         this.location.back();
+    }
+
+    toRGB(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result
+            ? {
+                  r: parseInt(result[1], 16),
+                  g: parseInt(result[2], 16),
+                  b: parseInt(result[3], 16),
+              }
+            : null;
+    }
+
+    getNavigationBarTextColor(color: string) {
+        const { r: a, g: b, b: c } = this.toRGB(color);
+        return 150 < a * 0.299 + b * 0.587 + c * 0.114 ? '#000' : '#fff';
     }
 }
