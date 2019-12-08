6a33c7ffa5480856a82031f436760a60b7d9c1f0
Five <f5.chjr@gmail.com>
2019-12-08 17:13:48 +0700

    fix(api->event): change type of 'date' from String to Date in EventSchema

--- api/src/models/event.model.ts
+++ api/src/models/event.model.ts
@@ -43,7 +43,7 @@ export const EventSchema = new Schema({
   organizer: String,
   schedules: [
     {
-      date: String,
+      date: Date,
       startTime: String,
       endTime: String,
       schedule: [
