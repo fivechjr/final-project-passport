8e153232e13778fe0c467562b4edd43f495448ba
Five <f5.chjr@gmail.com>
2019-12-08 09:46:21 +0700

    feat(app->shared): declare all components in SharedModule

--- app/src/app/@shared/shared.module.ts
+++ app/src/app/@shared/shared.module.ts
@@ -3,10 +3,35 @@ import { CommonModule } from '@angular/common';
 import { FrameComponent } from './frame/frame.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { ExpandButtonComponent } from './expand-button/expand-button.component';
+import { HorizontalEventCardComponent } from './horizontal-event-card/horizontal-event-card.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { VerticalEventCardComponent } from './vertical-event-card/vertical-event-card.component';
 
 @NgModule({
     imports: [CommonModule, RouterModule, IonicModule],
-    declarations: [FrameComponent],
-    exports: [FrameComponent],
+    declarations: [
+        ButtonComponent,
+        ExpandButtonComponent,
+        FrameComponent,
+        HorizontalEventCardComponent,
+        NavigationBarComponent,
+        PageTitleComponent,
+        ScheduleComponent,
+        VerticalEventCardComponent,
+    ],
+    exports: [
+        ButtonComponent,
+        ExpandButtonComponent,
+        FrameComponent,
+        HorizontalEventCardComponent,
+        NavigationBarComponent,
+        PageTitleComponent,
+        ScheduleComponent,
+        VerticalEventCardComponent,
+    ],
 })
 export class SharedModule {}
