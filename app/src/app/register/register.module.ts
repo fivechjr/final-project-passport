625d16baf3f0b7c36a7d9703b20a62500a7334d3
Five <f5.chjr@gmail.com>
2019-12-09 21:54:57 +0700

    feat(app->register): add RouterModule and SharedModule

--- app/src/app/register/register.module.ts
+++ app/src/app/register/register.module.ts
@@ -4,17 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { RegisterPageRoutingModule } from './register-routing.module';
-
 import { RegisterPage } from './register.page';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../@shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RegisterPageRoutingModule
-  ],
-  declarations: [RegisterPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule,
+        SharedModule,
+    ],
+    declarations: [RegisterPage],
 })
 export class RegisterPageModule {}
