653f00e66183572f484e5db4af10aefe39b30db4
Five <f5.chjr@gmail.com>
2019-12-09 17:03:57 +0700

    deploy(api): configure Terraform for Cloud Run

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
 provider "google" {
   credentials = file("~/Desktop/GCP.json")
 
-  project = "cu-passport"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = "final-project-passport"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-c"
 }
 
-resource "google_cloudbuild_trigger" "filename-trigger" {
-  trigger_template {
-    branch_name = "master"
-    repo_name   = "https://github.com/fivechjr/final-project-2019"
+resource "google_cloud_run_service" "default" {
+  name     = "cloud-run-final"
+  location = "asia-southeast1"
+
+  metadata {
+    namespace = "final-project"
+  }
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/final-project-passport/backend"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
   }
 }
