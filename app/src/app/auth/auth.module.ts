db1acc927a1cdd227927e8c7c0cdf2722a657dcd
Five <f5.chjr@gmail.com>
2019-12-09 21:53:15 +0700

    fix(app->auth): add RouterModule

--- app/src/app/auth/auth.module.ts
+++ app/src/app/auth/auth.module.ts
@@ -6,9 +6,16 @@ import { IonicModule } from '@ionic/angular';
 
 import { AuthPage } from './auth.page';
 import { SharedModule } from '../@shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, IonicModule, SharedModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule,
+        SharedModule,
+    ],
     declarations: [AuthPage],
 })
 export class AuthPageModule {}
