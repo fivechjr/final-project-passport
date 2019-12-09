23188ce8770df95461d3a8ecf9a98e2ed3e0c39d
Five <f5.chjr@gmail.com>
2019-12-09 20:02:12 +0700

    refactor(app->shared): add Input() to HorizontalEventCardComponent

--- app/src/app/@shared/horizontal-event-card/horizontal-event-card.component.ts
+++ app/src/app/@shared/horizontal-event-card/horizontal-event-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
     selector: 'app-horizontal-event-card',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./horizontal-event-card.component.scss'],
 })
 export class HorizontalEventCardComponent implements OnInit {
+    @Input() posterImage: string;
+    @Input() startDate: string;
+    @Input() endDate: string;
+    @Input() title: string;
+    @Input() faculty: string;
+    @Input() id: string;
+
+    public isEnded: boolean = false;
+    public day: string;
+    public month: string;
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        let date = dayjs(this.endDate).startOf('day');
+        let today = dayjs().startOf('day');
+        let diff = today.diff(date, 'day');
+        let startDate = dayjs(this.startDate);
+        this.day = startDate.format('D');
+        this.month = startDate.format('MMM');
+        if (date.isBefore(today) && diff >= 1) {
+            this.isEnded = true;
+        }
+    }
 }
