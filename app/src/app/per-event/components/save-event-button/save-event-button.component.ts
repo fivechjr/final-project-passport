e7f436890ab15403795d02e8dacd2aad1d042e59
Five <f5.chjr@gmail.com>
2019-12-09 10:14:22 +0700

    chore(app->per-event): clean up

--- app/src/app/per-event/components/save-event-button/save-event-button.component.ts
+++ app/src/app/per-event/components/save-event-button/save-event-button.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-save-event-button',
-  templateUrl: './save-event-button.component.html',
-  styleUrls: ['./save-event-button.component.scss'],
+    selector: 'app-save-event-button',
+    templateUrl: './save-event-button.component.html',
+    styleUrls: ['./save-event-button.component.scss'],
 })
 export class SaveEventButtonComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+    ngOnInit() {}
 }
