c75434966a50f21b1d68f337d122eb6adc7b8983
Five <f5.chjr@gmail.com>
2019-12-09 08:12:36 +0700

    feat(app->per-event): scaffold JoinEvent/SaveEventButtonComponent

---
+++ app/src/app/per-event/components/join-event-button/join-event-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-join-event-button',
+  templateUrl: './join-event-button.component.html',
+  styleUrls: ['./join-event-button.component.scss'],
+})
+export class JoinEventButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ app/src/app/per-event/components/save-event-button/save-event-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-event-button',
+  templateUrl: './save-event-button.component.html',
+  styleUrls: ['./save-event-button.component.scss'],
+})
+export class SaveEventButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
