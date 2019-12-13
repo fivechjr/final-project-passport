48f71739e542449e209802c4fd7f56bdf9bdb8fc
Five <f5.chjr@gmail.com>
2019-12-13 19:45:11 +0700

    style(app): adjust box-shadow on some elements

--- app/src/app/@shared/navigation-bar/navigation-bar.component.ts
+++ app/src/app/@shared/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-navigation-bar',
@@ -8,6 +9,17 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NavigationBarComponent implements OnInit {
     @Input() title: string = 'Page';
     @Input() backgroundColor: string = '#ffffff';
+    public isElevated = new BehaviorSubject(false);
+    @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+        const scrollTop = $event.target.scrollingElement.scrollTop;
+        if (scrollTop > 0 && !this.isElevated.getValue()) {
+            this.isElevated.next(true);
+        }
+
+        if (scrollTop === 0 && this.isElevated.getValue()) {
+            this.isElevated.next(false);
+        }
+    }
 
     constructor() {}
 
